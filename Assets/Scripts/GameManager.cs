17f55df4d17ecd2539150f167d41466653f86869
Bellec--Ancona Yoan <bellecanconayoan@gmail.com>
2020-12-06 14:57:37 +0100

    Added assets and Menu Done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject m_LooseMenu;
+    public GameObject m_VictoryMenu;
+    public GameObject m_PauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        m_PauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        m_PauseMenu.SetActive(false);
+    }
+
+    public void Victory()
+    {
+        m_VictoryMenu.SetActive(true);
+    }
+
+    public void Menu()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
